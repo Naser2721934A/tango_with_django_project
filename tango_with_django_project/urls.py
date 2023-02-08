9432b03eb00dc53d09c7cc5cb43d9bcfaa32e02b
Naser Alrashidi <2721934A@student.gla.ac.uk>
2023-02-09 02:49:15 +0500

    Eleventh commit

--- tango_with_django_project/urls.py
+++ tango_with_django_project/urls.py
@@ -22,6 +22,10 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
-    path('about',views.about, name='about'),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('rango/', views.index, name='index'),
+    path('about/',views.about, name='about'),
+    path('category/<slug:category_name_slug>/',
+         views.show_category, name='show_category'),
+
+]
+
